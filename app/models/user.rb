1e6e5e51a4d8387e54d172a5321641e1c0685632
Lawrence Carreon <l.carreon123@gmail.com>
2020-05-12 17:16:11 +1000

    testing heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, presence: true, uniqueness: true
     has_many :posts
     has_many :likes, :dependent => :destroy
-
-    validates :email, presence: true, uniqueness: true
 end
